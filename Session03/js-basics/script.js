214fe3d8e7419705d4e5a9a34b8c75d52656e45b
Arnav Gupta <arnav@codingblocks.com>
2020-03-27 14:27:12 +0530

    js basics
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Session03/js-basics/script.js
@@ -0,0 +1,52 @@
+console.log('hello')
+console.log(x) // var hoisting
+
+var x = 10
+let y = 11
+const z = 12
+
+
+// datatypes
+
+console.log(10, typeof 10)
+console.log('asdasd', typeof 'asdasd')
+console.log(true, typeof true)
+console.log([1,2,3], typeof [1,2,3])
+
+// arrays
+let arr = [1,2,3, 'a', 'b', 'c', true, false, [4,5,6]]
+
+// objects
+let obj = {
+    a: 10,
+    b: 'sfsfh',
+    c: true,
+    'd': 32,
+    'x y': 245,
+    p: [23,524,524,5],
+    q: {
+        dafd: 'adgsdg',
+        fssf: 245452
+    }
+}
+
+// functions 
+
+function f1 (a) {
+    console.log('you sent me', a)
+    return 2020
+}
+
+console.log(typeof f1)
+
+let f2 = f1() // typeof f2 ?
+let f3 = f1 // typeof f3 ?
+console.log('f2', typeof f2)
+console.log('f3', typeof f3)
+
+let f4 = function () {
+    console.log('running f4')
+    return 1
+}
+
+
