ccbfc0a485831c987d47ba4187247114086b7d03
Arnav Gupta <arnav@codingblocks.com>
2020-03-31 13:48:45 +0530

    read write in async mode
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Session04/node-basics/file-ops.js
+++ Session04/node-basics/file-ops.js
@@ -1,12 +1,18 @@
 const fs = require('fs')
 
 console.log('write start', Date.now())
-fs.writeFileSync('myfile.txt', 'Hello World')
-console.log('write end', Date.now())
+fs.writeFile('myfile.txt', 'fhdfhdfh', err => {
+  if (err) console.error(err)
+  console.log('actual write end', Date.now())
 
-console.log('read start', Date.now())
-const data = fs.readFileSync('myfile.txt')
-console.log('read end', Date.now())
+  console.log('read start', Date.now())
+  fs.readFile('myfile1.txt', (err, data) => {
+    if (err) throw err
+    console.log('read end', Date.now())
+    console.log(data.toString())
+    console.log('done')
 
-console.log(data.toString())
+  })
 
+
+})
